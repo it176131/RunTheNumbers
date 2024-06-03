9a3b7f233640c9a482d4ec25c46e9bf774a8abf3
Ian Thompson <ianiat11@gmail.com>
2024-06-02 19:27:23 -0500

    modified:   src/rtn/materials/interior.py 	- Added docstring for :class:`Framing`.

--- src/rtn/materials/interior.py
+++ src/rtn/materials/interior.py
@@ -10,7 +10,23 @@ class Electrical: ...
 class HVAC: ...
 
 
-class Framing: ...
+class Framing:
+    """Interior framing materials.
+
+    The two most common costs associated with framing are finishing an
+    interior space and building new construction. This class represents
+    framing an *interior space*, e.g., finishing an unfinished space or
+    adding partitions within rooms.
+
+    A basic wall consists of three parts:
+        1. Bottom Plate := A 2x4 piece of lumber running
+            horizontally along the bottom of the wall.
+        2. Top Plates := Two 2x4 pieces of lumber running horizontally
+            along the top of the wall.
+        3. Studs := 2x4 pieces of lumber running vertically every 16".
+    """
+
+    ...
 
 
 class Insulation: ...
