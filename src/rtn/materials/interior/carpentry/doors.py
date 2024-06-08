0fb6499eab1f3d6b0d09105691e97cee3b88310c
Ian Thompson <ianiat11@gmail.com>
2024-06-08 14:50:26 -0500

    modified:   src/rtn/materials/interior/carpentry/doors.py 	- Added scope of work notes regarding doors.

--- src/rtn/materials/interior/carpentry/doors.py
+++ src/rtn/materials/interior/carpentry/doors.py
@@ -5,6 +5,14 @@ Verify the following:
     - Does the door latch properly without unnecessary force?
     - Does the door open in the expected direction?
 
+If the existing doors are old or beat up, replace all the doors in the
+house. When purchasing doors, you have the option of purchasing just
+the door slab (the part that opens and closes) or a pre-hung door (the
+slab attached to the doorjamb). While many investors are happy to save
+a little money by just buying the slab, replacing a slab instead of the
+entire jamb can be more work for your carpenter. Additionally, you'll
+often end up paying extra to fix doors that aren't plumb or don't lock
+properly.
 """
 
 
