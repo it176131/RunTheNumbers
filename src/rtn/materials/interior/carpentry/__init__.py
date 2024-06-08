6c08df03d0f6be26a8431be8f226408dd7a653c5
Ian Thompson <ianiat11@gmail.com>
2024-06-08 14:30:59 -0500

    modified:   src/rtn/materials/interior/carpentry/__init__.py 	- Added docstring for carpentry package.

---
+++ src/rtn/materials/interior/carpentry/__init__.py
@@ -0,0 +1,21 @@
+"""Carpentry can make or break a house.
+
+Great carpentry makes a house feel well put together, while poor
+carpentry will make a buyer (or renter) question every part of the
+renovation. A good carpenter can serve in many different roles and can
+replace a lot of other contractors.
+
+But other than framing and decks, the bread and butter of carpentry
+work consists of things like door installations, window installations,
+and finish trim work. These are renovation tasks you'll likely have to
+perform in every one of your houses, so find a good carpenter sooner
+rather than later, and you'll find your renovations going more quickly
+and more smoothly than you can imagine. Bring in a bad carpenter, and
+all you'll find is frustration and a questionable looking renovation.
+
+This ``carpentry`` package will holld the bread-and-butter carpentry
+tasks:
+    - Install doors
+    - Install windows
+    - Finish trim
+"""
