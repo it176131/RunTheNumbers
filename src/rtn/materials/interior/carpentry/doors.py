17c4168b95600c2c042a0b35285de1ea9ea7b186
Ian Thompson <ianiat11@gmail.com>
2024-06-08 14:37:19 -0500

    modified:   src/rtn/materials/interior/carpentry/doors.py 	- Added docstring with general inspection tips for interior doors.

--- src/rtn/materials/interior/carpentry/doors.py
+++ src/rtn/materials/interior/carpentry/doors.py
@@ -1,1 +1,11 @@
+"""Ensure that every door works properly and as expected.
+
+Verify the following:
+    - Does the door open and close without obstructions?
+    - Does the door latch properly without unnecessary force?
+    - Does the door open in the expected direction?
+
+"""
+
+
 class Doors: ...
