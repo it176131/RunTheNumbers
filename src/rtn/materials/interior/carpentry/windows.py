a1b2d33d3ab14cb7c3f5e38f56f4c60966a9fc52
Ian Thompson <ianiat11@gmail.com>
2024-06-09 11:50:31 -0500

    modified:   src/rtn/materials/interior/carpentry/windows.py 	- Defined :class:`Window` with initial docstring.

--- src/rtn/materials/interior/carpentry/windows.py
+++ src/rtn/materials/interior/carpentry/windows.py
@@ -12,3 +12,23 @@ recommended you spend some time looking at different types of windows
 and getting a feel for the style of windows that are typical in the
 houses you plan to renovate.
 """
+
+from pydantic.main import BaseModel
+
+
+class Window(BaseModel):
+    """Window replacement.
+
+    Standard double-hung replacement windows come in a variety of sizes,
+    styles, and qualities. For most low- to mid-level renovations, it is
+    recommended to use stock vinyl windows that can be purchased from a
+    big-box store or ordered through a local window company.
+
+    You'll likely find that using the window company to install the
+    windows will cost quite a bit more than having a carpenter do the
+    installation. It is suggested to find a professional window
+    installer looking for side work at night or on weekends and pay him
+    an hourly rate for installation. Professional installers can
+    replace windows very quickly and paying hourly will keep the per-
+    window price to a minimum.
+    """
