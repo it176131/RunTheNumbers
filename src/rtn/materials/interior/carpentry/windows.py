9dfea81265a0005879fe75e039e52959126ec3f7
Ian Thompson <ianiat11@gmail.com>
2024-06-09 11:35:44 -0500

    modified:   src/rtn/materials/interior/carpentry/windows.py 	- Added module docstring.

---
+++ src/rtn/materials/interior/carpentry/windows.py
@@ -0,0 +1,9 @@
+"""Ensure that every window works properly and as expected.
+
+Verify the following:
+    - Does it open smoothly and easily?
+    - Does it remain open without assistance?
+    - Does it close and latch without any unnecessary force?
+    - Are the panes of glass free from cracks and moisture?
+    - Are there any gaps or openings around the window?
+"""
