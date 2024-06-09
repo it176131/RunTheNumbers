887a7ef7c9c4626729ee38f5a342eb39a2d73037
Ian Thompson <ianiat11@gmail.com>
2024-06-09 15:12:13 -0500

    modified:   src/rtn/materials/interior/carpentry/trim.py 	- Defined :class:`Trim`.

--- src/rtn/materials/interior/carpentry/trim.py
+++ src/rtn/materials/interior/carpentry/trim.py
@@ -6,3 +6,24 @@ Finish trim generally includes the following:
     - Door casing
     - Window casing
 """
+
+from pydantic.main import BaseModel
+
+
+class Trim(BaseModel):
+    """Finish trim.
+
+    Labor cost is based on square footage of the house while material
+    cost is based on the linear feet of actual trim to be installed.
+
+    To trim out a full house, many carpenters will charge based on the
+    square footage of the house as opposed to the specific amount of
+    trim you plan to use. This means you may pay the same price
+    regardless of whether you put crown molding in every room or none
+    of them.
+
+    Prices are typically for medium to large jobs that will take
+    several days or more, such as trimming an entire house. For smaller
+    jobs, you can base the labor price off the hourly price of your
+    carpenter.
+    """
