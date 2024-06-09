8e2c4bc204d9951b6b134e16b99c20dfb0b49fc9
Ian Thompson <ianiat11@gmail.com>
2024-06-09 17:02:29 -0500

    modified:   src/tests/test_materials/test_interior/test_carpentry/test_trim_cases.py 	- Renamed test cases.

--- src/tests/test_materials/test_interior/test_carpentry/test_trim_cases.py
+++ src/tests/test_materials/test_interior/test_carpentry/test_trim_cases.py
@@ -1,8 +1,8 @@
-def total_cost_one_foot() -> float:
+def len_one_foot() -> float:
     """A piece of trim one linear foot in length."""
     return 1.0
 
 
-def total_cost_97_cents() -> float:
+def cost_97_cents() -> float:
     """A piece of trim with a cost of $0.97 cents per linear foot."""
     return 0.97
