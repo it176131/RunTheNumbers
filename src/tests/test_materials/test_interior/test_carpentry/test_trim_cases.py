adad271af271fd4f16c0790c77d9547764df816f
Ian Thompson <ianiat11@gmail.com>
2024-06-09 16:59:14 -0500

    modified:   src/tests/test_materials/test_interior/test_carpentry/test_trim_cases.py 	- Added another test case for :method:`Trim.total_cost`.

--- src/tests/test_materials/test_interior/test_carpentry/test_trim_cases.py
+++ src/tests/test_materials/test_interior/test_carpentry/test_trim_cases.py
@@ -1,3 +1,8 @@
 def total_cost_one_foot() -> float:
     """A piece of trim one linear foot in length."""
     return 1.0
+
+
+def total_cost_97_cents() -> float:
+    """A piece of trim with a cost of $0.97 cents per linear foot."""
+    return 0.97
