53b4427ea08cf663153bc3a2ca79a487ae498753
Ian Thompson <ianiat11@gmail.com>
2024-06-09 11:37:27 -0500

    modified:   src/rtn/materials/interior/carpentry/windows.py 	- Added scope of work to module docs.

--- src/rtn/materials/interior/carpentry/windows.py
+++ src/rtn/materials/interior/carpentry/windows.py
@@ -6,4 +6,9 @@ Verify the following:
     - Does it close and latch without any unnecessary force?
     - Are the panes of glass free from cracks and moisture?
     - Are there any gaps or openings around the window?
+
+Windows come in all different shapes, sizes, and styles. It is highly
+recommended you spend some time looking at different types of windows
+and getting a feel for the style of windows that are typical in the
+houses you plan to renovate.
 """
