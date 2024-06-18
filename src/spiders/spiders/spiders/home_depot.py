058610ccbb89f084c09ada5ca58eafd30fb14f29
Ian Thompson <ianiat11@gmail.com>
2024-06-17 20:11:16 -0500

    modified:   src/spiders/spiders/spiders/home_depot.py 	- Updated inheritance of :class:`HomeDepot` from :class:`scrapy.crawler.Crawler` to :class:`scrapy.spiders.crawl.CrawlSpider`.

--- src/spiders/spiders/spiders/home_depot.py
+++ src/spiders/spiders/spiders/home_depot.py
@@ -1,8 +1,9 @@
-from scrapy.crawler import Crawler
+from scrapy.spiders.crawl import CrawlSpider
 
 
-class HomeDepot(Crawler):
+class HomeDepot(CrawlSpider):
     """A spider that crawls and parses the Home Depot website."""
 
     name: str = "homedepot"
     allowed_domains: list[str] | None = ["homedepot.com"]
+    start_urls: list[str] | None = None
